5d8734a1241d2df2692431f9ac1547b47bde9c4c
haramishra <haraprasad45@gmail.com>
2023-07-28 18:55:49 +0530

    spinner icon added (TODO: google and apple icon)

--- tixspot/src/components/icons.tsx
+++ tixspot/src/components/icons.tsx
@@ -3,10 +3,11 @@ import {
   Moon,
   SunMedium,
   Twitter,
+  Loader2,
   type Icon as LucideIcon,
-} from "lucide-react"
+} from "lucide-react";
 
-export type Icon = LucideIcon
+export type Icon = LucideIcon;
 
 export const Icons = {
   sun: SunMedium,
@@ -28,4 +29,5 @@ export const Icons = {
       ></path>
     </svg>
   ),
-}
+  spinner: Loader2,
+};
